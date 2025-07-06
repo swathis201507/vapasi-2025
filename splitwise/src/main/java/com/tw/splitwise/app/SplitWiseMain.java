499ff90fed5d632ef639dcf978d608428606bdb0
swathis <swathis201507@gmail.com>
2025-07-06 15:57:44 +0530

    Added optional to handle nulls and missing values in the input file

--- splitwise/src/main/java/com/tw/splitwise/app/SplitWiseMain.java
+++ splitwise/src/main/java/com/tw/splitwise/app/SplitWiseMain.java
@@ -1,6 +1,7 @@
 package com.tw.splitwise.app;
 
 import com.tw.splitwise.entity.Expense;
+import com.tw.splitwise.io.ExpenseParser;
 import com.tw.splitwise.io.ExpenseReader;
 
 import java.io.IOException;
@@ -11,12 +12,18 @@ public class SplitWiseMain {
 
     public static void main(String[] args) {
 
-        String filePath="/Users/swathis/IdeaProjects/vapasi-2025/splitwise/src/main/resources/expenses.txt";
-        ExpenseReader reader=new ExpenseReader(filePath);
+        String filePath = "/Users/swathis/IdeaProjects/vapasi-2025/splitwise/src/main/resources/expenses.txt";
+        ExpenseReader reader = new ExpenseReader(filePath);
+        ExpenseParser parser = new ExpenseParser();
 
-        try{
-            List<String> lines=reader.readLines();
-            List<Expense> expenses=new ArrayList<>();
+        try {
+            List<String> lines = reader.readLines();
+            List<Expense> expenses = new ArrayList<>();
+            for (String line : lines) {
+                parser.parse(line).ifPresent(expenses::add);
+            }
+
+            System.out.println(expenses);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- splitwise/src/main/java/com/tw/splitwise/io/ExpenseParser.java
+++ splitwise/src/main/java/com/tw/splitwise/io/ExpenseParser.java
@@ -8,26 +8,25 @@ import java.util.logging.Logger;
 public class ExpenseParser {
     private static final Logger logger = Logger.getLogger(ExpenseParser.class.getName());
 
-    public Expense parse(String line) {
+    public Optional<Expense> parse(String line) {
         String[] parts = line.split(" spent | for ", 4);
 
         if (parts.length < 4) {
             logger.info("Invalid format: " + line);
-            return null;
+            return Optional.empty();
         }
 
         try {
             String payer = parts[0].trim();
             double amount = Double.parseDouble(parts[1].trim());
             String[] beneficiaryArr = parts[3].split(",");
-            List<String> beneficiaries = Arrays.stream(beneficiaryArr)
-                    .map(String::trim)
-                    .toList();
-            return new Expense(payer, amount, beneficiaries);
+            List<String> beneficiaries = Arrays.stream(beneficiaryArr).map(String::trim).toList();
+            return Optional.of(new Expense(payer, amount, beneficiaries));
         } catch (NumberFormatException e) {
             logger.severe("Invalid amount in line: " + line);
-            return null;
+            return Optional.empty();
         }
     }
 }
+
 
