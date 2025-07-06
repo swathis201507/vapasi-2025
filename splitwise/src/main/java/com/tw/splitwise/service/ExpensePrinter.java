415c0d930364af0b1620b87169832a5b44d87eb5
swathis <swathis201507@gmail.com>
2025-07-06 16:19:02 +0530

    Added logic for printing the debts owed by all the users.

---
+++ splitwise/src/main/java/com/tw/splitwise/service/ExpensePrinter.java
@@ -0,0 +1,14 @@
+package com.tw.splitwise.service;
+
+import java.util.Map;
+
+public class ExpensePrinter {
+    public void printDebts(Map<String, Map<String, Double>> debtMap) {
+        for (var creditorEntry : debtMap.entrySet()) {
+            String creditor = creditorEntry.getKey();
+            for (var debtorEntry : creditorEntry.getValue().entrySet()) {
+                System.out.printf("%s owes %s amount- %.2f%n", debtorEntry.getKey(), creditor, debtorEntry.getValue());
+            }
+        }
+    }
+}
