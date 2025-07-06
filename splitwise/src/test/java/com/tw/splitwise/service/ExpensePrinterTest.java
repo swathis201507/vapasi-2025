c9d603e6b5d504fdd015ac4c035971435f4a9253
swathis <swathis201507@gmail.com>
2025-07-06 21:31:39 +0530

    Added testcases for printing the details of expenses owed to payer.

---
+++ splitwise/src/test/java/com/tw/splitwise/service/ExpensePrinterTest.java
@@ -0,0 +1,28 @@
+package com.tw.splitwise.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExpensePrinterTest {
+
+    @Test
+    void testDebtPrintsCorrectly() {
+        Map<String, Map<String, Double>> debtMap = new HashMap<>();
+        Map<String, Double> inner = new HashMap<>();
+        inner.put("B", 25.0);
+        inner.put("C", 25.0);
+        debtMap.put("A", inner);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        new ExpensePrinter().printDebts(debtMap);
+
+        String output = outContent.toString().trim();
+        assertTrue(output.contains("B owes A amount- 25.00"));
+        assertTrue(output.contains("C owes A amount- 25.00"));
+    }
+}
