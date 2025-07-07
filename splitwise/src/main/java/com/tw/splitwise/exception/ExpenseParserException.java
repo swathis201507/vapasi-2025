27ffce6354ffc6e6646666c583dc1f1810a4a9e0
swathis <swathis201507@gmail.com>
2025-07-07 22:27:36 +0530

    Added a custom exception to handle invalid file format and data

---
+++ splitwise/src/main/java/com/tw/splitwise/exception/ExpenseParserException.java
@@ -0,0 +1,7 @@
+package com.tw.splitwise.exception;
+
+public class ExpenseParserException extends Exception {
+    public ExpenseParserException(String message) {
+        super(message);
+    }
+}
