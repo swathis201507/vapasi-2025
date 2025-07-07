681de7d45f442767a131f937d3ccb696f4914742
swathis <swathis201507@gmail.com>
2025-07-07 22:37:02 +0530

    Cleaned test classes by removing empty spaces and imports not required.

--- splitwise/src/test/java/com/tw/splitwise/service/ExpensePrinterTest.java
+++ splitwise/src/test/java/com/tw/splitwise/service/ExpensePrinterTest.java
@@ -3,7 +3,6 @@ package com.tw.splitwise.service;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
 
