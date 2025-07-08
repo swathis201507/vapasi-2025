145213f3ce4ff88349aee36755efc495344a78ef
swathis <swathis201507@gmail.com>
2025-07-08 15:54:20 +0530

    Updated the Printer test class  to output to ByteOutputStream

--- splitwise/src/main/java/com/tw/splitwise/service/ExpensePrinter.java
+++ splitwise/src/main/java/com/tw/splitwise/service/ExpensePrinter.java
@@ -1,13 +1,16 @@
 package com.tw.splitwise.service;
 
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class ExpensePrinter {
+   // private static final Logger logger = Logger.getLogger(ExpensePrinter.class.getName());
+
     public void printDebts(Map<String, Map<String, Double>> debtMap) {
         for (var creditorEntry : debtMap.entrySet()) {
             String creditor = creditorEntry.getKey();
             for (var debtorEntry : creditorEntry.getValue().entrySet()) {
-                System.out.printf("%s owes %s amount- %.2f%n", debtorEntry.getKey(), creditor, debtorEntry.getValue());
+                System.out.printf("%s owes %s amount- %.2f%n", " "+debtorEntry.getKey(), creditor, debtorEntry.getValue());
             }
         }
     }
